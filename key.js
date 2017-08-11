2fcffc35d9f5fd01799155237be3263ccac1a137
JJ <jianan.beijing@gmail.com>
2017-08-10 22:47:12 -0700

    twitter key setup and init files

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'TFysrMBi1MvAhle1L8T8Aqtni',
+  consumer_secret: 'G2oZSwXbLA3smFa2wUsA8D5fmxhFbwhH3O5a6yrAgwQnQQfDwW',
+  access_token_key: '3519594198-59svqVrzy2Nnlf5tQsjdlSJWCccmNvnfsn5SxVK',
+  access_token_secret: 'NHvgVsLvRJbqkLEqsORbDB1p4nq7qlKXBubEM48sYxXoj',
+}
---
+++ liri.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+fs.readFile("key.js", "twitterKeys", function(){
+	if(err){
+		return console.log(err);
+	}
+})
