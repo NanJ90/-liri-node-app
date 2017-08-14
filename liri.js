7640799b5add1e133381f26822a871bc1ad6bde3
JJ <jianan.beijing@gmail.com>
2017-08-14 16:05:24 -0700

    all three works

--- liri.js
+++ liri.js
@@ -1,144 +1,151 @@
 var key = require('./key.js');
+var spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
+
 var twitterK = key.twitterKeys;
 var spotifyK = key.spotifyKeys;
-var arr= process.argv;
+var spotify = new spotify({
+    id: spotifyK.client_ID,
+    secret: spotifyK.client_Secret
+});
+
+var arr = process.argv;
 // console.log(arr);
 var arrLength = process.argv.length;
 // console.log("it is:" + arrLength);
-var request = require('request');
 
-var fs = require('fs');
 
-if (arrLength == 3){
-	if(arr[2] === "my-tweets"){
-		mytweet()
-	}
-	else if (arr[2] === "movie-this") {
-		var queryUrl = "http://www.omdbapi.com/?t=Mr.nobody&y=&plot=short&apikey=40e9cece";
-		request(queryUrl, function(error, response, body) {
-	  		if (!error && response.statusCode === 200) {
+if (arrLength == 3) {
+    if (arr[2] === "my-tweets") {
+        mytweet()
+    } else if (arr[2] === "movie-this") {
+        var queryUrl = "http://www.omdbapi.com/?t=Mr.nobody&y=&plot=short&apikey=40e9cece";
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
 
-	    	console.log("Title: " + JSON.parse(body).Title + ".");
-	    	console.log("Release Year: " + JSON.parse(body).Year + ".");
-	    	console.log("IMDB: " + JSON.parse(body).imdbRating + ".");
-	    	console.log("Rotton Tomatoes: " + JSON.parse(body).Ratings[1].Value + ".");
-	    	console.log("Country: " + JSON.parse(body).Country + ".");
-	    	console.log("Plot: " + JSON.parse(body).Plot + ".");
-	    	console.log("Cast: " + JSON.parse(body).Actors + ".");
+                console.log("Title: " + JSON.parse(body).Title + ".");
+                console.log("Release Year: " + JSON.parse(body).Year + ".");
+                console.log("IMDB: " + JSON.parse(body).imdbRating + ".");
+                console.log("Rotton Tomatoes: " + JSON.parse(body).Ratings[1].Value + ".");
+                console.log("Country: " + JSON.parse(body).Country + ".");
+                console.log("Plot: " + JSON.parse(body).Plot + ".");
+                console.log("Cast: " + JSON.parse(body).Actors + ".");
 
-			  }
-		});
-		
-	}
-	// else if (arr[2] === "spotify-this-song"){
-	// 	sign 
-	// }
-	else if (arr[2] === "do-what-it-says"){
-		fs.readFile("random.txt", "utf8", function(err,data){
-		if(err){
-			return(err);
-			}
-		console.log(data);
-			var dataArr = data.split(",");
-			console.log(dataArr);
-			// call spotify function
-		});
-	}
-	else{
-		console.log("nothing for you");
-	}
-}
+            }
+        });
 
-else if(arrLength > 3) {
-	if (arr[2] === "movie-this"){
-		movieThis();
-	}
+    } else if (arr[2] === "spotify-this-song") {
+        spotify.search({ type: 'track', query: '"The Sign"' }, function(error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            // console.log(JSON.stringify(data, null , 2));
+            console.log("URL: " + data.tracks.items[2].album.artists[0].external_urls.spotify);
+            console.log("Album: " + data.tracks.items[2].album.artists[0].name);
+            console.log("Artist: " + data.tracks.items[2].artists[0].name);
+        });
+    } else if (arr[2] === "do-what-it-says") {
+        fs.readFile("random.txt", "utf8", function(err, data) {
+            if (err) {
+                return (err);
+            }
+            console.log(data);
+            var dataArr = data.split(",");
+            console.log(dataArr);
+        });
+    } else {
+        console.log("nothing for you");
+    }
+} else if (arrLength > 3) {
+    if (arr[2] === "movie-this") {
+        movieThis();
+    } else if (arr[2] === "spotify-this-song") {
+        spotifySong();
+    } else {
+        console.log("nothing for you");
+    }
 
-	// else if (arr[2] === "spotify-this-song") {
-
-	// }
-	else{
-		console.log("nothing for you");
-	}
-	
-}
-
-else{
-	console.log("nothing for you");
+} else {
+    console.log("nothing for you");
 
 }
 
+// mytweet
+function mytweet() {
 
-function mytweet(){
+    var Twitter = require('twitter');
 
-var Twitter = require('twitter');
-
-var client = new Twitter({
-	consumer_key: twitterK.consumer_key,
-	consumer_secret: twitterK.consumer_secret,
-	access_token_key: twitterK.access_token_key,
-	access_token_secret: twitterK.access_token_secret
-});
+    var client = new Twitter({
+        consumer_key: twitterK.consumer_key,
+        consumer_secret: twitterK.consumer_secret,
+        access_token_key: twitterK.access_token_key,
+        access_token_secret: twitterK.access_token_secret
+    });
 
 
-var params = {
-    screen_name: 'Nannn_J',
-    count: 3 
+    var params = {
+        screen_name: 'Nannn_J',
+        count: 3
 
-};
+    };
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) 
-    for(var i = 0; i < tweets.length; i++){
-    		console.log(tweets[i].created_at, tweets[i].text);
-    	}
-    	
-	});
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error)
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at, tweets[i].text);
+            }
+
+    });
 }
 
-function movieThis(){
-	var movieName = "";
-		for (var i = 3; i < arrLength; i++) {
-		  if (i > 3 && i < arrLength) {
-		    movieName = movieName + "+" + arr[i];
+function movieThis() {
+    var movieName = "";
+    for (var i = 3; i < arrLength; i++) {
+        if (i > 3 && i < arrLength) {
+            movieName = movieName + "+" + arr[i];
 
-		  }
-		  else {
-		    movieName += arr[i];
-		  }
-		}
-	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-			console.log(queryUrl);
+        } else {
+            movieName += arr[i];
+        }
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    console.log(queryUrl);
 
-	request(queryUrl, function(error, response, body) {
-	  		if (!error && response.statusCode === 200) {
+    request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
 
-	    	console.log("Title: " + JSON.parse(body).Title + ".");
-	    	console.log("Release Year: " + JSON.parse(body).Year + ".");
-	    	console.log("IMDB: " + JSON.parse(body).imdbRating + ".");
-	    	console.log("Rotton Tomatoes: " + JSON.parse(body).Ratings[1].Value + ".");
-	    	console.log("Country: " + JSON.parse(body).Country + ".");
-	    	console.log("Plot: " + JSON.parse(body).Plot + ".");
-	    	console.log("Cast: " + JSON.parse(body).Actors + ".");
+            console.log("Title: " + JSON.parse(body).Title + ".");
+            console.log("Release Year: " + JSON.parse(body).Year + ".");
+            console.log("IMDB: " + JSON.parse(body).imdbRating + ".");
+            console.log("Rotton Tomatoes: " + JSON.parse(body).Ratings[1].Value + ".");
+            console.log("Country: " + JSON.parse(body).Country + ".");
+            console.log("Plot: " + JSON.parse(body).Plot + ".");
+            console.log("Cast: " + JSON.parse(body).Actors + ".");
 
-			  }
-		});
-	}
+        }
+    });
+}
 
-// mytweet();
-// function theSign(){
-// 	var spotify = require('node-spotify-api');
+// spofiySong();
+function spotifySong() {
+    var songName = '';
+    for (var i = 3; i < arrLength; i++) {
+        if (i > 3 && i < arrLength) {
+            songName = songName + "+" + arr[i];
 
-// 	var spotify = new spotify({
-// 		id:spotifyK.client_ID,
-// 		secret:spotifyK.client_Secret
-// 	});
-// // print out a bunch of data, some things shows [objects]
-// 	spotify.search({ type: 'track', query: 'the sign ace of base'}, function(error, data){
-// 		if(error){
-// 			return console.log(error);
-// 		}
-// 		// console.log(data.items.album.name);
-// 		console.log(data.artists[6]);
-// 	});
-// }
+        } else {
+            songName += arr[i];
+        }
+    }
+    // print out a bunch of data, some things shows [objects]
+    spotify.search({ type: 'track', query: songName }, function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        // console.log(JSON.stringify(data, null , 2));
+        console.log("URL: " + data.tracks.items[2].album.artists[0].external_urls.spotify);
+        console.log("Album: " + data.tracks.items[2].album.artists[0].name);
+        console.log("Artist: " + data.tracks.items[2].artists[0].name);
+    });
+}
