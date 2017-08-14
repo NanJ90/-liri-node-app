eaa16b6d392aa355b64ddfb75de8fb10d1791a8c
JJ <jianan.beijing@gmail.com>
2017-08-13 23:14:52 -0700

    twitter works,spotify working on

--- key.js
+++ key.js
@@ -1,8 +1,12 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
   consumer_key: 'TFysrMBi1MvAhle1L8T8Aqtni',
   consumer_secret: 'G2oZSwXbLA3smFa2wUsA8D5fmxhFbwhH3O5a6yrAgwQnQQfDwW',
   access_token_key: '3519594198-59svqVrzy2Nnlf5tQsjdlSJWCccmNvnfsn5SxVK',
   access_token_secret: 'NHvgVsLvRJbqkLEqsORbDB1p4nq7qlKXBubEM48sYxXoj',
+};
+
+exports.spotifyKeys = {
+	client_ID:'20a86943256c4f399a3a627b301f73bd',
+	client_Secret:'46ac5538e3b84b9cba7d0c4980e08d61'
 }
+
--- liri.js
+++ liri.js
@@ -1,6 +1,54 @@
-var fs = require("fs");
-fs.readFile("key.js", "twitterKeys", function(){
-	if(err){
-		return console.log(err);
-	}
-})
+var key = require('./key.js');
+var app = process.argv[2]
+if (app === "my-tweets"){
+	mytweet();
+}
+else if (app === "spotify-this-song") {
+	theSign()
+}
+else{
+	console.log("Working on it");
+}
+function mytweet(){
+var twitterK = key.twitterKeys;
+var Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: twitterK.consumer_key,
+	consumer_secret: twitterK.consumer_secret,
+	access_token_key: twitterK.access_token_key,
+	access_token_secret: twitterK.access_token_secret
+});
+
+
+var params = {
+    screen_name: 'Nannn_J',
+    count: 3 
+
+};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) 
+    for(var i = 0; i < tweets.length; i++){
+    		console.log(tweets[i].created_at, tweets[i].text);
+    	}
+    	
+	});
+}
+// mytweet();
+function theSign(){
+	var spotifyK = key.spotifyKeys;
+	var spotify = require('node-spotify-api');
+
+	var spotify = new spotify({
+		id:spotifyK.client_ID,
+		secret:spotifyK.client_Secret
+	});
+// print out a bunch of data, some things shows [objects]
+	spotify.search({ type: 'track', query: '"the sign"', limit: 5 }, function(error, data){
+		if(error){
+			return console.log(error);
+		}
+		console.log(data.tracks.items);
+	});
+}
